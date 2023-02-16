e3cf33757c3ef095f597787bbd6fcb7bc6b49dd2
neshb <naresh.barfa@launchventures.co>
2023-02-16 11:40:02 +0530

    create react app and run react in docker container

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello react app</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          created react app with help of docker
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
